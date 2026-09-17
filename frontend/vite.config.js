0af7b8d506fd3ed6eae7ffd8fed2dde725f22931
tanaypusalkar-jpg <tanaypusalkar@gmail.com>
2026-09-17 14:49:14 +0530

    updated vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-});
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      }
+    }
+  }
+})
